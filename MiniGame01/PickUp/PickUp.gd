aab3173b94d295d8613a4e891952c69ae9e14a0d
chris <sing.christian@gmail.com>
2018-09-09 18:19:09 +0200

    added particle animation butterfly

--- MiniGame01/PickUp/PickUp.gd
+++ MiniGame01/PickUp/PickUp.gd
@@ -5,6 +5,7 @@ onready var sprite = $Sprite
 onready var flower = $Flower
 onready var flower2 = $Flower2
 onready var animPlayer = $AnimationPlayer
+onready var particles = $Particles2D
 
 var textures = [preload('res://MiniGame01/Textures/Flowers/verwelkt.png'), preload('res://MiniGame01/Textures/Flowers/blume1.png'), preload('res://MiniGame01/Textures/Flowers/blume2.png'), preload('res://MiniGame01/Textures/Flowers/blume3.png')]
 var textureSwitch = false
@@ -18,3 +19,6 @@ func _process(delta):
 		flower2.texture = textures[randi()%3+1]
 		animPlayer.play("grow")
 		textureSwitch = true
+#START BUTTERFLYS AFTER GROW ANIMATION IS READY#
+func _on_AnimationPlayer_animation_finished(grow):
+	particles.emitting = true
