50d5a3a89f3ed11f3c7f177346a038a063b129f1
Chris <sing.christian@gmail.com>
2018-09-10 15:01:50 -0700

    added powerups at scene

---
+++ MiniGame01/PowerUp/PowerUp.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+onready var sprite = $Sprite
+var textures = [preload('res://MiniGame01/Textures/PickUps/item_boost.png'), preload('res://MiniGame01/Textures/PickUps/item_toolbox.png')]
+
+func _ready():
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
--- MiniGame01/PowerUps.gd
+++ MiniGame01/PowerUps.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+onready var powerup = [$PowerUp1, $PowerUp2, $PowerUp3, $PowerUp4, $PowerUp5, $PowerUp6, $PowerUp7, $PowerUp8, $PowerUp9, $PowerUp10, $PowerUp11, $PowerUp12, $PowerUp13]
+var obj = 0
+#Position Pickups are possible
+var positions = [350, 175, 0, -175, -350]
+var randomPos
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	for i in range(0, powerup.size()):
+		if obj == 0:
+			powerup[i].sprite.texture = powerup[i].textures[0]
+			obj = 1
+		else:
+			powerup[i].sprite.texture = powerup[i].textures[1]
+			obj = 0
+	#RANDOM POSITION Y POWERUPS
+	for obj in powerup:
+		randomPos  = randi()%5
+		obj.position.y = positions[randomPos]
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
